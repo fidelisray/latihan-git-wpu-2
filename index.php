564967bd0c8fbd7fb6c011479bae547246cfe92e
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-07-25 13:20:26 +0700

    Mengubah index.php mengganti title, h1, dan list

--- index.php
+++ index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coba GIT PHP</title>
+    <title>E Ticket Bus</title>
 </head>
 <body>
-    <h1>Coba GIT File PHP</h1>
+    <h1>Reservasi tiket bus online</h1>
     <h4><?= "Ini file latihan GIT" ?></h4>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id porro similique perspiciatis eaque rem alias dolores nam repellat, iusto eveniet!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo ipsam temporibus corrupti recusandae illo quis architecto cupiditate, perferendis aut sit.
-    </p>
     <ul>
-        <li>Fidelis Raymond</li>
-        <li>18.K1.0062</li>
+        <li>Sudiro Tungga Jaya</li>
+        <li>Rosalia Indah</li>
     </ul>
 </body>
 </html>
